149ce8de02be01aaa8842323450ea2a78f73b540
Kevin Huayllas <KevinOmega01@gmail.com>
2023-07-11 13:22:30 -0400

    adding axes

--- src/Canvas.js
+++ src/Canvas.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useState } from "react";
+import React, { useCallback, useEffect, useRef, useState } from "react";
 import "./canvas.css";
 import * as d3 from "d3";
 
@@ -6,6 +6,7 @@ const Canvas = () => {
   const [data, setData] = useState([]);
   const [baseTemperature, setBaseTemperature] = useState(0);
   const [isDrawed, setIsDrawed] = useState(false);
+  const containerRef = useRef();
 
   const fetchData = () => {
     fetch(
@@ -13,18 +14,49 @@ const Canvas = () => {
     )
       .then((response) => response.json())
       .then((data) => {
-        setData(data.monthlyVariance);
+        setData(
+          data.monthlyVariance.map((d) => {
+            const month = new Date(2000, d.month, 1);
+            return { ...d, month };
+          })
+        );
         setBaseTemperature(data.baseTemperature);
       });
   };
 
   const initData = useCallback(() => {
-    console.log(data);
+    const width = containerRef.current.getBoundingClientRect().width;
+    const height = containerRef.current.getBoundingClientRect().height;
+
+    const padding = 60;
+
     const svg = d3
       .select("#canvas-container")
       .append("svg")
       .attr("width", "100%")
       .attr("height", "100%");
+
+    const xScale = d3
+      .scaleLinear()
+      .domain([d3.min(data, (d) => d.year), d3.max(data, (d) => d.year)])
+      .nice()
+      .range([padding, width - padding]);
+
+    const yScale = d3
+      .scaleTime()
+      .range([height - padding, padding / 2])
+      .domain(d3.extent(data, (d) => d.month));
+
+    console.log(yScale(new Date(2000, 2, 1)));
+
+    const xAxis = d3.axisBottom(xScale);
+    const yAxis = d3.axisLeft(yScale).tickFormat(d3.timeFormat("%B"));
+
+    svg
+      .append("g")
+      .style("transform", `translateY(${height - padding}px)`)
+      .call(xAxis);
+    svg.append("g").style("transform", `translate(${padding}px)`).call(yAxis);
 
     svg.selectAll("rect").data(data).enter().append("rect").attr("cell");
   }, [data]);
@@ -48,7 +80,7 @@ const Canvas = () => {
         <h1>Monthly Global Land-Surface Temperature</h1>
         <h3>1753-2015 base temperature {baseTemperature}°</h3>
       </div>
-      <div id="canvas-container"></div>
+      <div id="canvas-container" ref={containerRef}></div>
     </div>
   );
 };
